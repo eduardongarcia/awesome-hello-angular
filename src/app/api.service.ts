8189420897290f2e25285d3278cc28d1667e5c65
egarcia <egarcia@ciandt.com>
2019-12-17 16:52:49 -0300

    Removing console.log

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
 import {throwError} from "rxjs";
 import {catchError, retry, tap} from "rxjs/operators";
@@ -8,18 +8,18 @@ import {catchError, retry, tap} from "rxjs/operators";
 })
 export class ApiService {
 
-  private SERVER_URL = "http://localhost:3000/products";
-
   public first: string;
   public prev: string;
   public next: string;
   public last: string;
+  private SERVER_URL = "http://localhost:3000/products";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   handleError(error: HttpErrorResponse) {
     let errorMessage;
-    if(error.error instanceof ErrorEvent) {
+    if (error.error instanceof ErrorEvent) {
       console.log(error.error);
       errorMessage = `Error: ${error.error.message}`;
     } else {
@@ -31,12 +31,12 @@ export class ApiService {
 
   parseLinkHeader(header) {
     if (!header) {
-      return ;
+      return;
     }
 
     let parts = header.split(',');
     const links = {};
-    parts.forEach( p => {
+    parts.forEach(p => {
       let section = p.split(';');
       const url = section[0].replace(/<(.*)>/, '$1').trim();
       const name = section[1].replace(/rel="(.*)"/, '$1').trim();
@@ -44,25 +44,23 @@ export class ApiService {
 
     });
 
-    this.first  = links["first"];
-    this.last   = links["last"];
-    this.prev   = links["prev"];
-    this.next   = links["next"];
+    this.first = links["first"];
+    this.last = links["last"];
+    this.prev = links["prev"];
+    this.next = links["next"];
   }
 
-  public sendGetRequest(){
+  public sendGetRequest() {
     return this.httpClient
-      .get(this.SERVER_URL, {  params: new HttpParams({fromString: "_page=1&_limit=4"}), observe: "response"})
+      .get(this.SERVER_URL, {params: new HttpParams({fromString: "_page=1&_limit=4"}), observe: "response"})
       .pipe(retry(3), catchError(this.handleError), tap(res => {
-        console.log(res.headers.get('Link'));
         this.parseLinkHeader(res.headers.get('Link'));
       }));
   }
 
-  public sendGetRequestToUrl(url: string){
-    return this.httpClient.get(url, { observe: "response"})
+  public sendGetRequestToUrl(url: string) {
+    return this.httpClient.get(url, {observe: "response"})
       .pipe(retry(3), catchError(this.handleError), tap(res => {
-        console.log(res.headers.get('Link'));
         this.parseLinkHeader(res.headers.get('Link'));
       }));
   }
