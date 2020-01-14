d1638ded18ccb851f3ed72d635a213e9513661c9
egarcia <egarcia@ciandt.com>
2020-01-14 13:22:18 -0300

    Dojo - aula 02

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
+import {throwError} from 'rxjs';
+import {catchError, retry, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,59 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public get(){
-    return this.httpClient.get(this.SERVER_URL);
+  public first: string = "";
+  public prev: string = "";
+  public next: string = "";
+  public last: string = "";
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  public sendGetRequest() {
+    return this.httpClient.get(this.SERVER_URL, {  params: new HttpParams({fromString: "_page=1&_limit=4"}), observe: "response"})
+      .pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+    }));
+  }
+
+  parseLinkHeader(header) {
+    if (header.length === 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    this.first  = links["first"];
+    this.last   = links["last"];
+    this.prev   = links["prev"];
+    this.next   = links["next"];
+  }
+
+  public sendGetRequestToUrl(url: string){
+    return this.httpClient.get(url, { observe: "response"}).pipe(retry(3),
+      catchError(this.handleError), tap(res => {
+        console.log(res.headers.get('Link'));
+        this.parseLinkHeader(res.headers.get('Link'));
+      }));
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ApiService} from '../api.service';
+import { HttpResponse } from '@angular/common/http';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   products = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.apiService.get().subscribe((data: any[]) => {
-      console.log(data);
-      this.products = data;
+
+    this.apiService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  public firstPage() {
+    this.products = [];
+    this.apiService.sendGetRequestToUrl(this.apiService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+  public previousPage() {
+
+    if (this.apiService.prev !== undefined && this.apiService.prev !== '') {
+      this.products = [];
+      this.apiService.sendGetRequestToUrl(this.apiService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+    }
+
+  }
+  public nextPage() {
+    if (this.apiService.next !== undefined && this.apiService.next !== '') {
+      this.products = [];
+      this.apiService.sendGetRequestToUrl(this.apiService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.apiService.sendGetRequestToUrl(this.apiService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
     });
   }
 }
